aabb84cb6ae5c931000e65356c40de40ec78936c
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-04-16 23:57:47 +0530

    first

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 
-import { signOut, useSession } from 'next-auth/react'
-import React from 'react'
+import { signOut, useSession } from 'next-auth/react';
+import React from 'react';
 
-function page() {
-    const {data:session}  = useSession();
+function Page() {
+    const { data: session } = useSession();
 
-  return (
-    <div>
-      <h1>This Is The Dashboard</h1> 
-
-
-      {session && <span>{session.user?.name}</span>}
-
-
-      <button onClick={()=>signOut()} className='rounded-lg bg-red-500 p-2'>Logout</button>   
-      </div>
-  )
+    return (
+        <div>
+            <h1>This Is The Dashboard</h1> 
+            {session && <span>{session.user?.name}</span>}
+            <button onClick={() => signOut()} className='rounded-lg bg-red-500 p-2'>Logout</button>   
+        </div>
+    );
 }
 
-export default page
+export default Page;
